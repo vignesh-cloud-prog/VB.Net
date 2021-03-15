a75f0676cd46e51eef40773ff7c020876871a50e
vignesh-cloud-prog <vigneshun80@gmail.com>
2021-03-15 10:07:48 +0530

    simple quiz application added

---
+++ SimpleQuiz/SimpleQuiz/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BtnQn1 = New System.Windows.Forms.Button()
+        Me.BtnQn2 = New System.Windows.Forms.Button()
+        Me.BtnQn3 = New System.Windows.Forms.Button()
+        Me.BtnQn4 = New System.Windows.Forms.Button()
+        Me.BtnQn5 = New System.Windows.Forms.Button()
+        Me.BtnScore = New System.Windows.Forms.Button()
+        Me.BtnRestart = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.LabelQn = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.BtnSubmit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(168, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Click the button to attemp"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(43, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(272, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "One question can be attempted only once"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(43, 95)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(230, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Each correct answer gets 20 marks"
+        '
+        'BtnQn1
+        '
+        Me.BtnQn1.Location = New System.Drawing.Point(46, 147)
+        Me.BtnQn1.Name = "BtnQn1"
+        Me.BtnQn1.Size = New System.Drawing.Size(75, 23)
+        Me.BtnQn1.TabIndex = 3
+        Me.BtnQn1.Text = "Qn1"
+        Me.BtnQn1.UseVisualStyleBackColor = True
+        '
+        'BtnQn2
+        '
+        Me.BtnQn2.Location = New System.Drawing.Point(133, 147)
+        Me.BtnQn2.Name = "BtnQn2"
+        Me.BtnQn2.Size = New System.Drawing.Size(75, 23)
+        Me.BtnQn2.TabIndex = 4
+        Me.BtnQn2.Text = "Qn2"
+        Me.BtnQn2.UseVisualStyleBackColor = True
+        '
+        'BtnQn3
+        '
+        Me.BtnQn3.Location = New System.Drawing.Point(227, 147)
+        Me.BtnQn3.Name = "BtnQn3"
+        Me.BtnQn3.Size = New System.Drawing.Size(75, 23)
+        Me.BtnQn3.TabIndex = 5
+        Me.BtnQn3.Text = "Qn3"
+        Me.BtnQn3.UseVisualStyleBackColor = True
+        '
+        'BtnQn4
+        '
+        Me.BtnQn4.Location = New System.Drawing.Point(326, 147)
+        Me.BtnQn4.Name = "BtnQn4"
+        Me.BtnQn4.Size = New System.Drawing.Size(75, 23)
+        Me.BtnQn4.TabIndex = 6
+        Me.BtnQn4.Text = "Qn4"
+        Me.BtnQn4.UseVisualStyleBackColor = True
+        '
+        'BtnQn5
+        '
+        Me.BtnQn5.Location = New System.Drawing.Point(430, 147)
+        Me.BtnQn5.Name = "BtnQn5"
+        Me.BtnQn5.Size = New System.Drawing.Size(75, 23)
+        Me.BtnQn5.TabIndex = 7
+        Me.BtnQn5.Text = "Qn5"
+        Me.BtnQn5.UseVisualStyleBackColor = True
+        '
+        'BtnScore
+        '
+        Me.BtnScore.Location = New System.Drawing.Point(528, 147)
+        Me.BtnScore.Name = "BtnScore"
+        Me.BtnScore.Size = New System.Drawing.Size(107, 23)
+        Me.BtnScore.TabIndex = 8
+        Me.BtnScore.Text = "Score"
+        Me.BtnScore.UseVisualStyleBackColor = True
+        '
+        'BtnRestart
+        '
+        Me.BtnRestart.Location = New System.Drawing.Point(166, 207)
+        Me.BtnRestart.Name = "BtnRestart"
+        Me.BtnRestart.Size = New System.Drawing.Size(107, 23)
+        Me.BtnRestart.TabIndex = 9
+        Me.BtnRestart.Text = "Restart"
+        Me.BtnRestart.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(310, 207)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(107, 23)
+        Me.BtnExit.TabIndex = 10
+        Me.BtnExit.Text = "Exit"
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.LabelQn)
+        Me.GroupBox1.Controls.Add(Me.GroupBox3)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(46, 286)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(699, 285)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'LabelQn
+        '
+        Me.LabelQn.AutoSize = True
+        Me.LabelQn.Location = New System.Drawing.Point(19, 46)
+        Me.LabelQn.Name = "LabelQn"
+        Me.LabelQn.Size = New System.Drawing.Size(51, 17)
+        Me.LabelQn.TabIndex = 2
+        Me.LabelQn.Text = "Label4"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.ComboBox1)
+        Me.GroupBox3.Location = New System.Drawing.Point(412, 121)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Select your anwer"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(21, 35)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 24)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Location = New System.Drawing.Point(19, 82)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(294, 182)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Select your answer"
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(17, 131)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "RadioButton4"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(17, 104)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(17, 77)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(17, 50)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'BtnSubmit
+        '
+        Me.BtnSubmit.Location = New System.Drawing.Point(805, 527)
+        Me.BtnSubmit.Name = "BtnSubmit"
+        Me.BtnSubmit.Size = New System.Drawing.Size(152, 23)
+        Me.BtnSubmit.TabIndex = 12
+        Me.BtnSubmit.Text = "Submit Answer"
+        Me.BtnSubmit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(983, 600)
+        Me.Controls.Add(Me.BtnSubmit)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.BtnRestart)
+        Me.Controls.Add(Me.BtnScore)
+        Me.Controls.Add(Me.BtnQn5)
+        Me.Controls.Add(Me.BtnQn4)
+        Me.Controls.Add(Me.BtnQn3)
+        Me.Controls.Add(Me.BtnQn2)
+        Me.Controls.Add(Me.BtnQn1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents BtnQn1 As System.Windows.Forms.Button
+    Friend WithEvents BtnQn2 As System.Windows.Forms.Button
+    Friend WithEvents BtnQn3 As System.Windows.Forms.Button
+    Friend WithEvents BtnQn4 As System.Windows.Forms.Button
+    Friend WithEvents BtnQn5 As System.Windows.Forms.Button
+    Friend WithEvents BtnScore As System.Windows.Forms.Button
+    Friend WithEvents BtnRestart As System.Windows.Forms.Button
+    Friend WithEvents BtnExit As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents LabelQn As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents BtnSubmit As System.Windows.Forms.Button
+
+End Class
---
+++ SimpleQuiz/SimpleQuiz/Form1.vb
@@ -0,0 +1,131 @@
+﻿Public Class Form1
+    Dim Score As Integer
+
+    Function load()
+        BtnQn1.Enabled = True
+        BtnQn2.Enabled = True
+        BtnQn3.Enabled = True
+        BtnQn4.Enabled = True
+        BtnQn5.Enabled = True
+
+        GroupBox2.Visible = False
+        GroupBox3.Visible = False
+        Score = 0
+
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        load()
+
+    End Sub
+
+    Private Sub BtnQn1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnQn1.Click
+        GroupBox1.Text = BtnQn1.Text
+        GroupBox2.Visible = True
+        GroupBox3.Visible = False
+
+        LabelQn.Text = "Who is president of india"
+        RadioButton1.Text = "Narendra Modi"
+        RadioButton2.Text = "Nagaraja s g"
+        RadioButton3.Text = "Yadiyurappa"
+        RadioButton4.Text = "Nithin gadkari"
+        BtnQn1.Enabled = False
+
+
+    End Sub
+
+    Private Sub BtnQn2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnQn2.Click
+        GroupBox1.Text = BtnQn2.Text
+        GroupBox2.Visible = True
+        GroupBox3.Visible = False
+        LabelQn.Text = "Who is president of india"
+        RadioButton1.Text = "Narendra Modi"
+        RadioButton2.Text = "Nagaraja s g"
+        RadioButton3.Text = "Yadiyurappa"
+        RadioButton4.Text = "Nithin gadkari"
+        BtnQn2.Enabled = False
+    End Sub
+
+    Private Sub BtnQn3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnQn3.Click
+        GroupBox1.Text = BtnQn3.Text
+        GroupBox2.Visible = True
+        GroupBox3.Visible = False
+        LabelQn.Text = "Who is president of india"
+        RadioButton1.Text = "Narendra Modi"
+        RadioButton2.Text = "Nagaraja s g"
+        RadioButton3.Text = "Yadiyurappa"
+        RadioButton4.Text = "Nithin gadkari"
+        BtnQn3.Enabled = False
+    End Sub
+
+    Private Sub BtnQn4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnQn4.Click
+        GroupBox1.Text = BtnQn4.Text
+        GroupBox2.Visible = False
+        GroupBox3.Visible = True
+        LabelQn.Text = "OS stands for"
+        ComboBox1.Items.Clear()
+        ComboBox1.Items.Add("Operating System")
+        ComboBox1.Items.Add("Operator overloading")
+        ComboBox1.Items.Add("Open souce")
+        ComboBox1.Items.Add("order of significance ")
+        BtnQn4.Enabled = False
+
+    End Sub
+
+    Private Sub BtnQn5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnQn5.Click
+        GroupBox2.Visible = False
+        GroupBox3.Visible = True
+        GroupBox1.Text = BtnQn5.Text
+        LabelQn.Text = "OS stands for"
+        ComboBox1.Items.Clear()
+        ComboBox1.Items.Add("Operating System")
+        ComboBox1.Items.Add("Operator overloading")
+        ComboBox1.Items.Add("Open souce")
+        ComboBox1.Items.Add("order of significance ")
+        BtnQn5.Enabled = False
+    End Sub
+
+    Private Sub BtnScore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnScore.Click
+        MsgBox("Score is " & Score)
+
+    End Sub
+
+    Private Sub BtnRestart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnRestart.Click
+        load()
+
+    End Sub
+
+    Private Sub BtnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnExit.Click
+        End
+
+    End Sub
+
+    Private Sub BtnSubmit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnSubmit.Click
+        Select Case GroupBox1.Text
+            Case "Qn1"
+                If RadioButton1.Checked Then
+                    Score = Score + 20
+                End If
+            Case "Qn2"
+                If RadioButton2.Checked Then
+                    Score = Score + 20
+                End If
+            Case "Qn3"
+                If RadioButton3.Checked Then
+                    Score = Score + 20
+                End If
+            Case "Qn4"
+                If ComboBox1.SelectedItem = "Operating System" Then
+                    Score = Score + 20
+                End If
+            Case "Qn5"
+                If ComboBox1.SelectedItem = "Open souce" Then
+                    Score = Score + 20
+                End If
+        End Select
+        GroupBox1.Text = ""
+        GroupBox2.Visible = False
+        GroupBox3.Visible = False
+
+    End Sub
+End Class
